09e3664001d909096759eaa597adec3475581695
subins-kumar004 <subinsuresh802@gmail.com>
2026-05-23 20:50:47 +0530

    Updated PHP record programs

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
+<html>
+<head>
+    <title>PHP Programs</title>
+</head>
+
+<body>
+
+<h1>PHP Programs</h1>
+
+<ul>
+
 <?php
-echo "Hello from PHP!";
+
+$files = glob("*.php");
+
+foreach ($files as $file)
+{
+    if ($file != "index.php")
+    {
+        echo "<li><a href='$file'>$file</a></li>";
+    }
+}
+
 ?>
+
+</ul>
+
+<h2>Folders</h2>
+
+<ul>
+
+<?php
+
+$folders = glob("*", GLOB_ONLYDIR);
+
+foreach ($folders as $folder)
+{
+    echo "<li><a href='$folder'>$folder</a></li>";
+}
+
+?>
+
+</ul>
+
+</body>
+</html>
